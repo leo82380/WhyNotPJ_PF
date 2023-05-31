70913741bbac5a24aaccc491cb459cc63572e12a
youtaehew <youtaehew123@gmail.com>
2023-05-31 20:04:54 +0900

    Pet
    123

---
+++ WhyNot_PF/Assets/You/Pet_Controller.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet_Controller : MonoBehaviour
+{
+    Animator ani;
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
