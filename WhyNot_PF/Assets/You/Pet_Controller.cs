30d19a024f2e3e9637ec7bb8d36f5702c2be7d59
youtaehew <youtaehew123@gmail.com>
2023-06-01 19:43:51 +0900

    Pet

--- WhyNot_PF/Assets/You/Pet_Controller.cs
+++ WhyNot_PF/Assets/You/Pet_Controller.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class Pet_Controller : MonoBehaviour
 {
     Animator ani;
+    Transform ParentPlayer;
+    Transform ChildPlayer;
+    [SerializeField]
+    float Speed = 1f;
     void Start()
     {
         ani = GetComponent<Animator>();
+        ParentPlayer = GameObject.FindWithTag("Player").transform;
+        ChildPlayer = ParentPlayer.GetChild(2);
     }
 
     
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, ChildPlayer.position, Speed);
+        if(ParentPlayer.position.x> transform.position.x)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+        else if (ParentPlayer.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
+        }
+    }
+
+    public void Die()
+    {
+        ani.Play("1_Dead");
     }
 }
