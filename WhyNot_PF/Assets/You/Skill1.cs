139445e6893bffd1e05fcf301bb18e8fa1bae03c
youtaehew <youtaehew123@gmail.com>
2023-06-21 20:27:49 +0900

    effect

--- WhyNot_PF/Assets/You/Skill1.cs
+++ WhyNot_PF/Assets/You/Skill1.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 using DG.Tweening;
 public class Skill1 : MonoBehaviour
 {
+    public static Skill1 Instance;
     SpriteRenderer playerImage;
+
+    [ColorUsage(true,true)]
+    private Color originColor;
+    [ColorUsage(true,true),SerializeField]
+    private Color changeColor;
+
     private void Awake()
     {
         playerImage = GameObject.Find("Pet").GetComponent<SpriteRenderer>();
+        originColor = playerImage.material.GetColor("_Color");
+        
     }
     void Start()
     {
-        playerImage.DOColor(Color., 0.5f);
-        
+        playerImage.material.DOColor(changeColor, "_Color", 1);
     }
     public void delete()
     {
-        playerImage.DOColor(Color.white, 1f);
+        playerImage.material.DOColor(originColor, "_Color", .5f);
         Destroy(gameObject);
     }
 
